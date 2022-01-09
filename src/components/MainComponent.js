b8db4c8473f7a11a35a139d8a2f3b11360716b9e
Eberlouedd <kyacini@yahoo.fr>
2022-01-09 11:50:10 +0100

    aboutcomp

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import {LEADERS} from './shared/leaders';
 import Footer from './FooterComponent';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 
 class Main extends Component{
@@ -25,11 +26,6 @@ class Main extends Component{
       leaders: LEADERS
     }
   }
-
-  onDishselected(dish){
-    this.setState({selectedDish: dish});
-    this.setState({comments: dish.comments});
- }
 
 
   render(){
@@ -46,9 +42,15 @@ class Main extends Component{
 
     const DishWithId = ({match}) => {
       return(
-        <Dishdetail dish={match.params.id} comments={this.state.comments}/>
+        <div className='container'>
+          <div className='row'>
+            <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}/>
+          </div>
+        </div>
       )
     }
+
 
     return (
       <div className="App">
@@ -58,8 +60,9 @@ class Main extends Component{
           <Switch>
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-            <Route path="/menu/:dish" component={DishWithId}/>
+            <Route path="/menu/:dishId" component={DishWithId}/>
             <Route exact path='/contactus' component={Contact} />
+            <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
             <Redirect to='/home' />
           </Switch>
           <Footer />
