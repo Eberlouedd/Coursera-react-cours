a127f8caa4a04d543274b5bf6f98876ed205be5c
Eberlouedd <kyacini@yahoo.fr>
2022-01-10 03:12:08 +0100

    redux added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import Main from './components/MainComponent'
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore'
 
 
 
 class App extends Component{
-
+  
   render(){
+    const store = ConfigureStore();
     return (
-      
+      <Provider store={store}>
         <div className="App">
             <Main />
         </div>
+      </Provider>
     );
   }
 }
